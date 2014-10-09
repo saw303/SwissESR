5b23e1d1b3d8b85fdbcb9c10f46dbfea459d5de7
Silvio Wangler <silvio.wangler@gmail.com>
2014-10-09 08:05:14 +0200

    Modify SVG

---
+++ src/main/groovy/io/wangler/esr/SvgParser.groovy
@@ -0,0 +1,24 @@
+package io.wangler.esr
+
+/**
+ * @author Silvio Wangler
+ * @since 0.3
+ */
+class SvgParser {
+
+    public static void main(String[] args) {
+
+        def resource = SvgParser.class.getResource('/esr.svg')
+
+        def root = new XmlParser().parseText(resource.text)
+
+        def allRecords = root.depthFirst().findAll { it.name().qualifiedName == 'tspan' && it.text()}
+
+        for (record in allRecords) {
+            println "id: ${record.@id}, element:${record.name().qualifiedName}, value ${record.text()}"
+            //record.value = 'yolo'
+        }
+
+        new XmlNodePrinter(new PrintWriter(System.out)).print(root)
+    }
+}
